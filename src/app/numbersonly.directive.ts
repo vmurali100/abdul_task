49b05a4f519b186c63be861142e13012b60ab8d6
vmurali100 <vmurali100@gmail.com>
2020-04-03 15:50:06 +0530

    Added

--- src/app/numbersonly.directive.ts
+++ src/app/numbersonly.directive.ts
@@ -9,22 +9,22 @@ export class NumbersonlyDirective {
   @HostListener("input", ["$event"]) onInputChange(event) {
     const initalValue = this.el.nativeElement.value;
     let pattern = /^[-+]?\d+(\.\d+)?$/;
-    if (event.data !== "." && event.data !== "-") {
-      if (pattern.test(initalValue)) {
-        this.el.nativeElement.value = initalValue;
-      } else {
+    if (event.data !== null) {
+      if (event.data !== "." && event.data !== "-") {
+        if (pattern.test(initalValue)) {
+          this.el.nativeElement.value = initalValue;
+        } else {
+          this.el.nativeElement.value = initalValue.substr(
+            0,
+            initalValue.length - 1
+          );
+        }
+      } else if (initalValue.lastIndexOf("-") > 0) {
         this.el.nativeElement.value = initalValue.substr(
           0,
           initalValue.length - 1
         );
-        // str = str.substr(0, str.length - 1);
       }
     }
-
-    // if (val != "-" && val !== "." && val) {
-    //   this.el.nativeElement.value = initalValue.replace(/[^0-9]*/g, "");
-    // } else {
-    //   this.el.nativeElement.value = initalValue;
-    // }
   }
 }
