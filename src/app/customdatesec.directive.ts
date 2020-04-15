477238154f829229acd5f16416480246e863fcdb
vmurali100 <vmurali100@gmail.com>
2020-04-15 18:12:01 +0530

    2nd Directive

--- src/app/customdatesec.directive.ts
+++ src/app/customdatesec.directive.ts
@@ -10,27 +10,66 @@ export class CustomdatesecDirective {
     let initialValue = this.el.nativeElement.value;
     let pattern = /^[0-9]*$/;
     if (initialValue.length <= 23) {
-      if (pattern.test(initialValue)) {
+      if (initialValue.length <= 4 && pattern.test(event.data)) {
+        this.el.nativeElement.value = initialValue;
+      } else if (
+        (initialValue.length == 5 || initialValue.length == 8) &&
+        event.data == "-"
+      ) {
+        this.el.nativeElement.value = initialValue;
+      } else if (
+        initialValue.length > 5 &&
+        initialValue.length < 8 &&
+        pattern.test(event.data)
+      ) {
+        this.el.nativeElement.value = initialValue;
+      } else if (
+        initialValue.length > 8 &&
+        initialValue.length < 11 &&
+        pattern.test(event.data)
+      ) {
+        this.el.nativeElement.value = initialValue;
+      } else if (initialValue.length == 11 && event.data == "T") {
+        this.el.nativeElement.value = initialValue;
+      } else if (
+        initialValue.length > 11 &&
+        initialValue.length < 14 &&
+        pattern.test(event.data)
+      ) {
+        this.el.nativeElement.value = initialValue;
+      } else if (
+        (initialValue.length == 14 ||
+          initialValue.length == 17 ||
+          initialValue.length == 20) &&
+        (event.data == ":" || event.data == ".")
+      ) {
+        this.el.nativeElement.value = initialValue;
+      } else if (
+        initialValue.length > 14 &&
+        initialValue.length < 17 &&
+        pattern.test(event.data)
+      ) {
+        this.el.nativeElement.value = initialValue;
+      } else if (
+        initialValue.length > 17 &&
+        initialValue.length < 20 &&
+        pattern.test(event.data)
+      ) {
+        this.el.nativeElement.value = initialValue;
+      } else if (initialValue.length > 20 && pattern.test(event.data)) {
         this.el.nativeElement.value = initialValue;
       } else {
-        if (event.data == "T") {
-          if (initialValue.length == 9) {
-            this.el.nativeElement.value = initialValue;
-          } else if (initialValue.length < 9 || initialValue.length > 9) {
-            this.el.nativeElement.value = initialValue.substring(
-              0,
-              initialValue.length - 1
-            );
-          }
-        } else {
-          console.log(initialValue);
-        }
+        this.correctInput(initialValue);
       }
     } else {
-      this.el.nativeElement.value = initialValue.substring(
-        0,
-        initialValue.length - 1
-      );
+      this.correctInput(initialValue);
     }
+  }
+
+  correctInput(initialValue) {
+    this.el.nativeElement.value = initialValue.substring(
+      0,
+      initialValue.length - 1
+    );
   }
 }
