7b68ed78bd019dcef29e4f0665a2f27147d36f54
vmurali100 <vmurali100@gmail.com>
2020-04-15 20:32:50 +0530

    4h

--- src/app/customdatesec.directive.ts
+++ src/app/customdatesec.directive.ts
@@ -9,8 +9,9 @@ export class CustomdatesecDirective {
   @HostListener("input", ["$event"]) onInputChange(event) {
     let initialValue = this.el.nativeElement.value;
     let pattern = /^[0-9]*$/;
-    // if (initialValue.length <= 23) {
-    if (initialValue.length <= 26) {
+    let spattern = this.el.nativeElement.getAttribute("spattern");
+    let characters = this.el.nativeElement.getAttribute("ncar");
+    if (initialValue.length <= Number(characters)) {
       if (initialValue.length <= 4 && pattern.test(event.data)) {
         this.el.nativeElement.value = initialValue;
       } else if (
@@ -57,8 +58,26 @@ export class CustomdatesecDirective {
         pattern.test(event.data)
       ) {
         this.el.nativeElement.value = initialValue;
-      } else if (initialValue.length > 20 && pattern.test(event.data)) {
+      } else if (
+        initialValue.length > 20 &&
+        spattern == "3rd" &&
+        pattern.test(event.data)
+      ) {
         this.el.nativeElement.value = initialValue;
+      } else if (initialValue.length > 20 && spattern == "4th") {
+        if (
+          initialValue.length > 20 &&
+          initialValue.length < 24 &&
+          pattern.test(event.data)
+        ) {
+          this.el.nativeElement.value = initialValue;
+        } else if (initialValue.length == 24 && event.data == "+") {
+          this.el.nativeElement.value = initialValue;
+        } else if (initialValue.length > 24 && pattern.test(event.data)) {
+          this.el.nativeElement.value = initialValue;
+        } else {
+          this.correctInput(initialValue);
+        }
       } else {
         this.correctInput(initialValue);
       }
